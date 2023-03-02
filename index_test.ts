772d61a555aa759766c9c39172bc64814566f842
stefangohlke <gohlke.stefan@yahoo.de>
2023-03-02 18:58:48 +0100

    Add missing startServer test

--- index_test.ts
+++ index_test.ts
@@ -1,5 +1,20 @@
 import { assertEquals } from './deps.ts'
+import { startServer } from './index.ts'
 
-Deno.test('Example test', () => {
-   assertEquals('test', 'test')
+const jsonContentTypeHeader = {
+   'content-type': 'application/json; charset=UTF-8',
+}
+
+Deno.test('Calling startServer should return expected result', async () => {
+   const abortController = new AbortController()
+   startServer(() =>
+      new Response(JSON.stringify({ message: 'test' }), {
+         headers: jsonContentTypeHeader,
+      }), { port: 7035, signal: abortController.signal })
+
+   const response = await fetch('http://localhost:7035/')
+   assertEquals(response.status, 200)
+   const responseJson = await response.json()
+   assertEquals(responseJson.message, 'test')
+   abortController.abort()
 })
