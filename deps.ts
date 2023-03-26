d5b8de09c6fb7f124e72e9bccacbf05f20815c67
stefangohlke <gohlke.stefan@yahoo.de>
2023-03-26 20:52:42 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export type {
    Handler,
    ServeInit,
-} from 'https://deno.land/std@0.178.0/http/server.ts'
-export { serve } from 'https://deno.land/std@0.178.0/http/server.ts'
-export { assertEquals } from 'https://deno.land/std@0.178.0/testing/asserts.ts'
+} from 'https://deno.land/std@0.181.0/http/server.ts'
+export { serve } from 'https://deno.land/std@0.181.0/http/server.ts'
+export { assertEquals } from 'https://deno.land/std@0.181.0/testing/asserts.ts'
