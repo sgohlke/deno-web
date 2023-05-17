1b906f04d614704975d2559ab64acd44b2edf644
stefangohlke <gohlke.stefan@yahoo.de>
2023-05-17 20:47:52 +0200

    Add basic webserver

---
+++ webserver.ts
@@ -0,0 +1,69 @@
+import { ServeInit } from './deps.ts'
+import {
+   JSON_CONTENT_TYPE_HEADER,
+   returnDataResponse,
+   startServer,
+} from './index.ts'
+
+export interface ServerOptions {
+   serveInit?: ServeInit
+   enableCORS?: boolean
+}
+
+export class Webserver {
+   serverOptions: ServerOptions
+   abortController?: AbortController
+
+   constructor(options?: ServerOptions) {
+      this.serverOptions = options ??
+         {
+            serveInit: {
+               signal: (this.abortController = new AbortController()).signal,
+            },
+         }
+
+      if (!this.serverOptions.serveInit) {
+         this.serverOptions.serveInit = {
+            signal: (this.abortController = new AbortController()).signal,
+         }
+      } else if (!this.serverOptions.serveInit.signal) {
+         this.serverOptions.serveInit.signal =
+            (this.abortController = new AbortController()).signal
+      }
+   }
+
+   startWebserver(): void {
+      startServer(this.handler, this.serverOptions?.serveInit)
+   }
+
+   closeWebserver(): void {
+      if (this.abortController) {
+         this.abortController.abort()
+      }
+   }
+
+   handler: (request: Request) => Response = (request) => {
+      const responseHeaders = new Headers()
+      const origin = request.headers.get('origin')
+      if (origin && this.serverOptions.enableCORS) {
+         responseHeaders.set('Access-Control-Allow-Origin', origin)
+      }
+
+      const { pathname } = new URL(request.url)
+
+      if (request.method === 'OPTIONS') {
+         return new Response(undefined, { headers: responseHeaders })
+      } //TODO: Replace with routing functionality
+      else if (pathname === '/' || pathname === '') {
+         responseHeaders.append(
+            'content-type',
+            JSON_CONTENT_TYPE_HEADER['content-type'],
+         )
+         return returnDataResponse({ message: 'Welcome!' }, responseHeaders)
+      }
+      return new Response(undefined, {
+         headers: responseHeaders,
+         status: 404,
+      })
+   }
+}
---
+++ webserver_test.ts
@@ -0,0 +1,159 @@
+import { assertEquals } from './deps.ts'
+import { Webserver } from './webserver.ts'
+
+Deno.test('Calling startServer from Webserver with CORS enabled should return expected result', async () => {
+   const webserver = new Webserver({
+      serveInit: {
+         port: 7035,
+      },
+      enableCORS: true,
+   })
+   webserver.startWebserver()
+
+   // Test root route
+   let response = await fetch('http://localhost:7035/')
+   assertEquals(response.status, 200)
+   let responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test root route without /
+   response = await fetch('http://localhost:7035')
+   assertEquals(response.status, 200)
+   responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test CORS reflection
+   response = await fetch('http://localhost:7035/', {
+      method: 'Options',
+      headers: { 'origin': 'testOrigin' },
+   })
+   assertEquals(response.status, 200)
+   assertEquals(
+      response.headers.get('Access-Control-Allow-Origin'),
+      'testOrigin',
+   )
+   await response.body?.cancel()
+
+   // Test unavailable route
+   response = await fetch('http://localhost:7035/unknown')
+   assertEquals(response.status, 404)
+   responseJson = await response.text()
+   assertEquals(responseJson.message, undefined)
+   webserver.closeWebserver()
+})
+
+Deno.test('Calling startServer from Webserver with CORS disabled should return expected result', async () => {
+   const abortController = new AbortController()
+   const webserver = new Webserver({
+      serveInit: {
+         port: 7035,
+         signal: abortController.signal,
+      },
+   })
+   webserver.startWebserver()
+
+   // Test root route
+   let response = await fetch('http://localhost:7035/')
+   assertEquals(response.status, 200)
+   let responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test root route without /
+   response = await fetch('http://localhost:7035')
+   assertEquals(response.status, 200)
+   responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test CORS reflection
+   response = await fetch('http://localhost:7035/', {
+      method: 'Options',
+      headers: { 'origin': 'testOrigin' },
+   })
+   assertEquals(response.status, 200)
+   assertEquals(
+      response.headers.get('Access-Control-Allow-Origin'),
+      null,
+   )
+   await response.body?.cancel()
+
+   // Test unavailable route
+   response = await fetch('http://localhost:7035/unknown')
+   assertEquals(response.status, 404)
+   responseJson = await response.text()
+   assertEquals(responseJson.message, undefined)
+   // Does not do anything as serveInit.signal was provided and Webserver does not handle external abort signals
+   webserver.closeWebserver()
+   abortController.abort()
+})
+
+Deno.test('Calling startServer from Webserver without options should return expected result', async () => {
+   const webserver = new Webserver()
+   webserver.startWebserver()
+
+   // Test root route
+   let response = await fetch('http://localhost:8000/')
+   assertEquals(response.status, 200)
+   let responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test root route without /
+   response = await fetch('http://localhost:8000')
+   assertEquals(response.status, 200)
+   responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test CORS reflection
+   response = await fetch('http://localhost:8000/', {
+      method: 'Options',
+      headers: { 'origin': 'testOrigin' },
+   })
+   assertEquals(response.status, 200)
+   assertEquals(
+      response.headers.get('Access-Control-Allow-Origin'),
+      null,
+   )
+   await response.body?.cancel()
+
+   // Test unavailable route
+   response = await fetch('http://localhost:8000/unknown')
+   assertEquals(response.status, 404)
+   responseJson = await response.text()
+   assertEquals(responseJson.message, undefined)
+   webserver.closeWebserver()
+})
+
+Deno.test('Calling startServer from Webserver without serveInit should return expected result', async () => {
+   const webserver = new Webserver({ enableCORS: false })
+   webserver.startWebserver()
+
+   // Test root route
+   let response = await fetch('http://localhost:8000/')
+   assertEquals(response.status, 200)
+   let responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test root route without /
+   response = await fetch('http://localhost:8000')
+   assertEquals(response.status, 200)
+   responseJson = await response.json()
+   assertEquals(responseJson.message, 'Welcome!')
+
+   // Test CORS reflection
+   response = await fetch('http://localhost:8000/', {
+      method: 'Options',
+      headers: { 'origin': 'testOrigin' },
+   })
+   assertEquals(response.status, 200)
+   assertEquals(
+      response.headers.get('Access-Control-Allow-Origin'),
+      null,
+   )
+   await response.body?.cancel()
+
+   // Test unavailable route
+   response = await fetch('http://localhost:8000/unknown')
+   assertEquals(response.status, 404)
+   responseJson = await response.text()
+   assertEquals(responseJson.message, undefined)
+   webserver.closeWebserver()
+})
