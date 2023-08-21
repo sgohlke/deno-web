8a047742b452fdb26714ffed86455434abc75fa3
sgohlke <gohlke.stefan@yahoo.de>
2023-08-21 21:45:57 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { serve } from 'https://deno.land/std@0.193.0/http/server.ts'
+export { serve } from 'https://deno.land/std@0.199.0/http/server.ts'
 export {
    assert,
    assertArrayIncludes,
    assertEquals,
    assertThrows,
-} from 'https://deno.land/std@0.193.0/testing/asserts.ts'
+} from 'https://deno.land/std@0.199.0/testing/asserts.ts'
