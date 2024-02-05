045845b36c50a3ecd79c48eda5794933ec5f5e92
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 20:53:07 +0100

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export {
    assert,
-} from 'https://deno.land/std@0.212.0/assert/assert.ts'
+} from 'https://deno.land/std@0.214.0/assert/assert.ts'
 
 export {
    assertEquals,
-} from 'https://deno.land/std@0.212.0/assert/assert_equals.ts'
+} from 'https://deno.land/std@0.214.0/assert/assert_equals.ts'
